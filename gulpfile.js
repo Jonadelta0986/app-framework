53e87b3469c6a44794b589b0548b071966cc8d1a
Praveen Ravichandran <me@pravinba9495.dev>
2020-09-17 12:04:03 -0400

    Lint update

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,12 @@ const minify = require('gulp-minify');
 const clean = require('gulp-clean');
 
 gulp.task('clean', () => {
-	return gulp.src('dist/*.js').pipe(clean());
+  return gulp.src('dist/*.js').pipe(clean());
 });
 
 gulp.task('minify', () => {
-	return gulp.src('dist/aheeva.js').pipe(minify({ext: '.min.js'})).pipe(gulp.dest('dist'));
+  return gulp
+    .src('dist/aheeva.js')
+    .pipe(minify({ ext: '.min.js' }))
+    .pipe(gulp.dest('dist'));
 });
